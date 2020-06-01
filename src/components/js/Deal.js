9b895b4a70f9414625cb836f9203ce91c4181cae
JoshDuymel <joshduymel@gmail.com>
2020-06-01 16:37:14 +1200

    added color change

--- src/components/js/Deal.js
+++ src/components/js/Deal.js
@@ -2,10 +2,23 @@ import React from 'react';
 import "../css/Deal.css";
 
 export default function DealCont(props) {
+
+    const barColor = () => {
+        if(props.dealType == "ACQUISITION") {
+            return "#862BB0"
+        } else if(props.dealType == "LOAN") {
+            return "#183BB7"
+        } else {
+            return "#34BB7A"
+        }
+    }
+    
     return (
         <div className='dealBgCont'>
             <div className='dealCont'>
-                <h1 className='jobType'>{props.dealType}</h1>
+                <div>
+                    <h1 className='jobType' style={{borderBottom: `6px solid ${barColor()}`}}>{props.dealType}</h1>
+                </div>
                 <div>
                     <h3 className='roleType'>CUSTOMER:</h3>
                     <h6>{props.customerName}</h6>
--- src/components/js/KeyFig.js
+++ src/components/js/KeyFig.js
@@ -2,11 +2,14 @@ import React from 'react';
 import "../css/KeyFig.css";
 
 export default function KeyFig(props) {
+    // const barColor = () => {
+    //     if()
+    // }
     return (
         <div className='keyFigCont'>
             <h1>{props.date}</h1>
             <svg>
-                <rect width='3vw' height='4px'/>
+                <rect fill="blue" width='3vw' height='4px'/>
             </svg>
             <h2>{props.title}</h2>
         </div>
