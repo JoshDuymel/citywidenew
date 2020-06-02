59a3618cf03c7ed4c67abe1ff04528a574864d40
JoshDuymel <joshduymel@gmail.com>
2020-06-02 15:43:15 +1200

    updated deals page

--- src/pages/js/Deals.js
+++ src/pages/js/Deals.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+// import { Link } from "react-router-dom";
+
+import "../css/Deals.css";
+
+import dealData from '../../data/deals.json';
+import DealCont from '../../components/js/Deal';
 
 export default function Deals() {
     return (
-        <div>
+
+        <div className='dealsCont'>
+            <div className="featureStaticCont" title="a featureBox">
+                <div className="featureCont">
+                    <h1>Our references.</h1>
+                    <h4 className="sDText" >SCROLL DOWN</h4>
+                </div>
+            </div>
+
+            <div className="dealsSectionCont" title="the latest deals">
+                <div className="titleCont">
+                    <h5>02</h5>
+                    <h5>OUR LATEST DEALS</h5>
+                </div>
+                <div className='dealsContentCont'>
+                    {dealData.dealData.map(i => <DealCont dealType={i.dealType} customerName={i.customerName} counterName={i.counterName}/>)}
+                </div>
+                {/* <button className="linkCta" onClick={loadMore()} >LOAD MORE</button> */}
+            </div>
             
         </div>
     )
